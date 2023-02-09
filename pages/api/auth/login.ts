723a3cb81ef9d4f01ae1dadca6bd0afb75a2e512
Stelios Mavrokoukoulakis <stelios.mav@mind-alliance.com>
2023-02-09 16:09:03 +0200

    hotfix

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -9,10 +9,6 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const createdUser = await createUser({ id: 1, email: "kaliopi@yahoo.com" });
-  // const users = await getUsers();
-  console.log(createdUser);
-
   const body = req.body;
   if (!body.email || !body.password) {
     return res.status(400).json({ data: "First or last name not found" });
