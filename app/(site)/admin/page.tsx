3e41936b88cf46c5a00963ee4b83aed5f9f3c7e0
Stelios Mavrokoukoulakis <stelios.mav@mind-alliance.com>
2023-02-18 17:21:57 +0200

    Postgres first commit

--- app/(site)/admin/page.tsx
+++ app/(site)/admin/page.tsx
@@ -1,11 +1,22 @@
+import prisma from "@/lib/prisma";
+import { getUsers } from "@/lib/prisma/users";
+
 export const metadata = {
   title: "About",
 };
 
-export default function About() {
+export default async function About() {
+  const res = await getUsers();
+  const users = "data" in res ? res.data : [];
   return (
     <main className="h-full bg-red-200 ">
-      <h1>Admin Page</h1>
+      <h1 className="text-2xl font-bold">Admin Page</h1>
+      {users.map((user) => (
+        <article>
+          <h2 className="text-lg font-bold">{user.name}</h2>
+          <p>{user.email}</p>
+        </article>
+      ))}
     </main>
   );
 }
--- lib/prisma/index.ts
+++ lib/prisma/index.ts
@@ -5,7 +5,6 @@ declare global {
 }
 
 const prisma = globalThis.prisma || new PrismaClient();
-// if (process.env.NODE_ENV !== "production")
-globalThis.prisma = prisma;
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
 
 export default prisma;
